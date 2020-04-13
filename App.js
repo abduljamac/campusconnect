a3d48a43028c2b849c963afa23fee6a87cbb6ec6
abduljamac <abduljama001@gmail.com>
2020-04-13 15:14:39 +0100

    project re-setup

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
-import AppNavigator from './navigation/AppNavigator'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-
-export default class App extends Component {
-  render() {
-    return (
-      <AppNavigator />
-    )
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- util/constants.js
+++
@@ -1,5 +0,0 @@
-export const SIGNUP = `/signup`;
-export const LOGIN = `/login`;
-export const USER = `/user`
-export const UPLOAD_IMAGE = `/user/image`;
-export const GET_FREELANCERS = `/freelancers`
